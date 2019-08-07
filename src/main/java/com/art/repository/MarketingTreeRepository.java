d3b8f964e524154b9d0fd6f7aa7b2659c95d6bbb
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2019-08-07 14:29:15 +0500

    Исправление. "Не активен" -> "NO_ACTIVE"

--- src/main/java/com/art/repository/MarketingTreeRepository.java
+++ src/main/java/com/art/repository/MarketingTreeRepository.java
@@ -21,7 +21,7 @@ public interface MarketingTreeRepository extends JpaRepository<MarketingTree, Bi
     Page<MarketingTree> findAll(Specification<MarketingTree> specification, Pageable pageable);
 
     @Query(value = "SELECT new com.art.model.supporting.dto.MarketingTreeDTO(" +
-            "inv.partnerId, inv.id, inv.kin, MIN(ic.dateGivedCash), 'Не активен', 0, 0) " +
+            "inv.partnerId, inv.id, inv.kin, MIN(ic.dateGivedCash), 'NO_ACTIVE', 0, 0) " +
             "FROM InvestorsCash ic " +
             "JOIN ic.investor inv " +
             "WHERE inv.login <> 'investor-demo' AND inv.partnerId IS NOT NULL " +
