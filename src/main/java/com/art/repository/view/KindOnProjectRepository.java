adba556ff4f9b931835c05fc0915b6b0079ffc8f
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-05-05 22:59:23 +0500

    Добавлены и реализованы методы работы с с представлением "Доля в проекте"

---
+++ src/main/java/com/art/repository/view/KindOnProjectRepository.java
@@ -0,0 +1,18 @@
+package com.art.repository.view;
+
+import com.art.model.supporting.view.KindOnProject;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+@Repository
+public interface KindOnProjectRepository extends JpaRepository<KindOnProject, Long> {
+
+    List<KindOnProject> findByLogin(String login);
+
+}
---
+++ src/main/java/com/art/service/view/KindOnProjectService.java
@@ -0,0 +1,26 @@
+package com.art.service.view;
+
+import com.art.model.supporting.view.KindOnProject;
+import com.art.repository.view.KindOnProjectRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+@Service
+public class KindOnProjectService {
+
+    private final KindOnProjectRepository kindOnProjectRepository;
+
+    public KindOnProjectService(KindOnProjectRepository kindOnProjectRepository) {
+        this.kindOnProjectRepository = kindOnProjectRepository;
+    }
+
+    public List<KindOnProject> findByInvestorLogin(String login) {
+        return kindOnProjectRepository.findByLogin(login);
+    }
+
+}
