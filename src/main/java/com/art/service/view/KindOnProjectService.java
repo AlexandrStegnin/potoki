1e1dc8ca1832effb25a89991e25c2fa9eb78b26e
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-05-15 11:33:54 +0500

    Добавлена сортировка по дате покупки

--- src/main/java/com/art/service/view/KindOnProjectService.java
+++ src/main/java/com/art/service/view/KindOnProjectService.java
@@ -20,7 +20,7 @@ public class KindOnProjectService {
     }
 
     public List<KindOnProject> findByInvestorLogin(String login) {
-        return kindOnProjectRepository.findByLogin(login);
+        return kindOnProjectRepository.findByLoginOrderByBuyDate(login);
     }
 
 }
