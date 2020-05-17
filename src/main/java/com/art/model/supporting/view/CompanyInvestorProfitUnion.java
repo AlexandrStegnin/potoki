a692420a00fe987ab1a8e8332791880fe9cbeac1
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-05-17 21:49:48 +0500

    Добавлен класс для формирования заработка компании в целом и поклиентно

---
+++ src/main/java/com/art/model/supporting/view/CompanyInvestorProfitUnion.java
@@ -0,0 +1,43 @@
+package com.art.model.supporting.view;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+/**
+ * Сущность для отображения представления заработок компании по годам и по всем инвесторам
+ *
+ * @author Alexandr Stegnin
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CompanyInvestorProfitUnion {
+
+    private int yearSale;
+
+    private BigDecimal profit;
+
+    private String login;
+
+    private BigDecimal investorProfit;
+
+    public int getYearSale() {
+        return yearSale;
+    }
+
+    public BigDecimal getProfit() {
+        return profit;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public BigDecimal getInvestorProfit() {
+        return investorProfit;
+    }
+}
