618d16a6de38a1256e3353807adcb41d9ca9e65e
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-05-22 18:12:27 +0500

    Добавлена проверка. Если передаём логин пользователя, значит доступ из админки, иначе сам пользователь.

--- src/main/java/com/art/service/view/InvestorProfitService.java
+++ src/main/java/com/art/service/view/InvestorProfitService.java
@@ -1,10 +1,12 @@
 package com.art.service.view;
 
+import com.art.config.SecurityUtils;
 import com.art.model.supporting.view.InvestorProfit;
 import com.art.repository.view.InvestorProfitRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @author Alexandr Stegnin
@@ -20,6 +22,9 @@ public class InvestorProfitService {
     }
 
     public List<InvestorProfit> findByLogin(String login) {
+        if (Objects.isNull(login)) {
+            login = SecurityUtils.getUsername();
+        }
         return investorProfitRepository.findByLoginOrderByYearSale(login);
     }
 
--- src/main/java/com/art/service/view/KindOnProjectService.java
+++ src/main/java/com/art/service/view/KindOnProjectService.java
@@ -1,10 +1,12 @@
 package com.art.service.view;
 
+import com.art.config.SecurityUtils;
 import com.art.model.supporting.view.KindOnProject;
 import com.art.repository.view.KindOnProjectRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @author Alexandr Stegnin
@@ -20,6 +22,9 @@ public class KindOnProjectService {
     }
 
     public List<KindOnProject> findByInvestorLogin(String login) {
+        if (Objects.isNull(login)) {
+            login = SecurityUtils.getUsername();
+        }
         return kindOnProjectRepository.findByLoginOrderByBuyDate(login);
     }
 
