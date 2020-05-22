5ae9087d0d7cb037865d436581634ec837542f5b
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-05-22 18:13:01 +0500

    Изменения. Заменили хардкорный логин на параметр.

--- src/main/java/com/art/controllers/view/CompanyInvestorProfitUnionController.java
+++ src/main/java/com/art/controllers/view/CompanyInvestorProfitUnionController.java
@@ -6,6 +6,7 @@ import com.art.model.supporting.view.InvestorProfit;
 import com.art.service.view.CompanyProfitService;
 import com.art.service.view.InvestorProfitService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -30,25 +31,23 @@ public class CompanyInvestorProfitUnionController {
     }
 
     @GetMapping(path = "/union-profit")
-    public List<CompanyInvestorProfitUnion> findByLogin() {
+    public List<CompanyInvestorProfitUnion> findByLogin(@RequestParam(required = false) String login) {
         List<CompanyInvestorProfitUnion> profitUnions = new ArrayList<>();
         List<CompanyProfit> companyProfits = companyProfitService.findAll();
-        List<InvestorProfit> investorProfits = investorProfitService.findByLogin("investor007");
+        List<InvestorProfit> investorProfits = investorProfitService.findByLogin(login);
         companyProfits.forEach(companyProfit -> {
             CompanyInvestorProfitUnion union = new CompanyInvestorProfitUnion();
             union.setYearSale(companyProfit.getYearSale());
             union.setProfit(companyProfit.getProfit());
             profitUnions.add(union);
         });
-        profitUnions.forEach(profitUnion -> {
-            investorProfits.stream()
-                    .filter(investorProfit -> investorProfit.getYearSale() == profitUnion.getYearSale())
-                    .findFirst()
-                    .ifPresent(profit -> {
-                        profitUnion.setLogin(profit.getLogin());
-                        profitUnion.setInvestorProfit(profit.getProfit());
-                    });
-        });
+        profitUnions.forEach(profitUnion -> investorProfits.stream()
+                .filter(investorProfit -> investorProfit.getYearSale() == profitUnion.getYearSale())
+                .findFirst()
+                .ifPresent(profit -> {
+                    profitUnion.setLogin(profit.getLogin());
+                    profitUnion.setInvestorProfit(profit.getProfit());
+                }));
         return profitUnions;
     }
 
