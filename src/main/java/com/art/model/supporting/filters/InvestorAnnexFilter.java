e39cb6c7d0327b59f24978df21164994b54a3cb4
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-06-04 10:40:13 +0500

    Добавлен фильтр для приложений к договорам инвесторов

---
+++ src/main/java/com/art/model/supporting/filters/InvestorAnnexFilter.java
@@ -0,0 +1,14 @@
+package com.art.model.supporting.filters;
+
+import lombok.Data;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+@Data
+public class InvestorAnnexFilter {
+
+    private String investor;
+
+}
