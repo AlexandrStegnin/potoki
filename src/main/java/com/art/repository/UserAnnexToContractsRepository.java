4982819d9dbe93a1d00dc9783525293846866b20
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-06-04 12:50:42 +0500

    Добавлен метод получения приложения к договору по id пользователя и названию файла.

---
+++ src/main/java/com/art/repository/UserAnnexToContractsRepository.java
@@ -0,0 +1,19 @@
+package com.art.repository;
+
+import com.art.model.UsersAnnexToContracts;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigInteger;
+import java.util.List;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+@Repository
+public interface UserAnnexToContractsRepository extends JpaRepository<UsersAnnexToContracts, BigInteger> {
+
+    List<UsersAnnexToContracts> findByUserIdAndAnnex_AnnexName(BigInteger userId, String annexName);
+
+}
