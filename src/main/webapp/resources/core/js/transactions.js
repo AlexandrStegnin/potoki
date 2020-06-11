867a3f5c4e76acf53348115ea89cf9dedcee75c5
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-06-11 23:58:45 +0500

    Добавлен метод отката транзакции категории СОЗДАНИЕ (CREATE) в скрипт

--- src/main/webapp/resources/core/js/transactions.js
+++ src/main/webapp/resources/core/js/transactions.js
@@ -9,9 +9,17 @@ jQuery(document).ready(function ($) {
     $('.tx-show').on('click', function (e) {
         e.preventDefault();
         getTransactionCash($(this).attr('data-tx-id'));
+    });
+    $('.rollback-tx').on('click', function () {
+        rollbackTransaction($(this).attr('data-tx-id'))
     })
 });
 
+/**
+ * Получить список денег по id транзакции
+ *
+ * @param txId id транзакции
+ */
 function getTransactionCash(txId) {
     let token = $("meta[name='_csrf']").attr("content");
     let header = $("meta[name='_csrf_header']").attr("content");
@@ -42,6 +50,42 @@ function getTransactionCash(txId) {
         });
 }
 
+/**
+ * Откатить транзакцию по id
+ *
+ * @param txId id транзакции
+ */
+function rollbackTransaction(txId) {
+    let token = $("meta[name='_csrf']").attr("content");
+    let header = $("meta[name='_csrf_header']").attr("content");
+
+    let logDTO = {
+        id: txId
+    };
+    showLoader();
+    $.ajax({
+        type: "POST",
+        contentType: "application/json;charset=utf-8",
+        url: "transactions/rollback",
+        data: JSON.stringify(logDTO),
+        dataType: 'json',
+        timeout: 0,
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader(header, token);
+        }
+    })
+        .done(function (data) {
+            showMessage(data);
+        })
+        .fail(function (e) {
+            closeLoader();
+            showMessage(e.toLocaleString());
+        })
+        .always(function () {
+            closeLoader();
+        });
+}
+
 function createCashTable(cashList) {
     let cashTable = $('#cash-table');
     let tableBody = cashTable.find('tbody');
@@ -60,4 +104,13 @@ function createRow(cash) {
         $('<td>').text(cash.dateGivenCash),
         $('<td>').text((cash.givenCash).toLocaleString())
     );
+}
+
+function showMessage(message) {
+    let modal = $('#msg-modal');
+    modal.find('#msg').html(message);
+    modal.modal('show');
+    setTimeout(function () {
+        $('#msg-modal').modal('hide');
+    }, 3000);
 }
