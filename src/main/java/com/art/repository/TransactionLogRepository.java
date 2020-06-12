535486292b43ee3a8c96a443c9b83c9ad7a18c06
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-06-12 16:53:02 +0500

    Добавлен метод поиска списка транзакций, которые содержат сумму инвестора

--- src/main/java/com/art/repository/TransactionLogRepository.java
+++ src/main/java/com/art/repository/TransactionLogRepository.java
@@ -1,11 +1,14 @@
 package com.art.repository;
 
+import com.art.model.InvestorsCash;
 import com.art.model.TransactionLog;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * @author Alexandr Stegnin
@@ -15,5 +18,7 @@ import org.springframework.stereotype.Repository;
 public interface TransactionLogRepository extends JpaRepository<TransactionLog, Long> {
 
     Page<TransactionLog> findAll(Specification<TransactionLog> filter, Pageable pageable);
+
+    List<TransactionLog> findByInvestorsCashesContains(InvestorsCash cash);
 
 }
