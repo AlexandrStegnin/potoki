ddc359121873eadd39aaa9d9f9f81befb241884d
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-06-13 17:08:48 +0500

    Добавлено поле id транзакции.

--- src/main/java/com/art/model/supporting/dto/TransactionLogDTO.java
+++ src/main/java/com/art/model/supporting/dto/TransactionLogDTO.java
@@ -25,12 +25,15 @@ public class TransactionLogDTO {
 
     private boolean rollbackEnabled;
 
+    private Long blockedFrom;
+
     public TransactionLogDTO(TransactionLog entity) {
         this.id = entity.getId();
         this.createdBy = entity.getCreatedBy();
         this.txDate = getFormattedTxDate(entity.getTxDate());
         this.type = entity.getType().getTitle();
         this.rollbackEnabled = entity.isRollbackEnabled();
+        this.blockedFrom = entity.getBlockedFrom() == null ? null : entity.getBlockedFrom().getId();
     }
 
     private String getFormattedTxDate(Date txDate) {
