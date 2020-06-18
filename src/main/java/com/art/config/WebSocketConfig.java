6681f01300c48b7c3b389cc347831e1ad762d9cb
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-06-18 23:09:00 +0500

    Добавлена конечная точка для использования без SockJS

--- src/main/java/com/art/config/WebSocketConfig.java
+++ src/main/java/com/art/config/WebSocketConfig.java
@@ -18,6 +18,7 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/turn", "/status");
         registry.addEndpoint("/turn", "/status").withSockJS();
     }
 
