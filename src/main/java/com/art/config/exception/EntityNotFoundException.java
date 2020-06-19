38eea0237e2648f282c7a298dcc316f550c4340d
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-06-19 20:58:12 +0500

    Добавлен класс для исключений

---
+++ src/main/java/com/art/config/exception/EntityNotFoundException.java
@@ -0,0 +1,13 @@
+package com.art.config.exception;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+public class EntityNotFoundException extends RuntimeException {
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+
+}
