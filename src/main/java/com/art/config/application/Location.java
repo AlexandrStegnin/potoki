2932dbbccab1ed5bc222a99ed0d7809a375edc11
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-06-19 20:57:42 +0500

    Добавлены пути для работы с видами клиентов

--- src/main/java/com/art/config/application/Location.java
+++ src/main/java/com/art/config/application/Location.java
@@ -14,4 +14,13 @@ public class Location {
 
     public static final String URL_TRANSACTIONS_CASH = URL_TRANSACTIONS + "/cash";
 
+    public static final String CLIENT_TYPES = "/client/types";
+
+    public static final String CLIENT_TYPES_CREATE = CLIENT_TYPES + "/create";
+
+    public static final String CLIENT_TYPES_UPDATE = CLIENT_TYPES + "/update";
+
+    public static final String CLIENT_TYPES_DELETE = CLIENT_TYPES + "/delete";
+
+
 }
