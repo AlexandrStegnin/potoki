21e4bc38e4bdf39165ea73306719a844a152ce87
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-06-19 20:47:36 +0500

    Добавлены и реализованы методы для работы с видами клиентов

---
+++ src/main/java/com/art/repository/ClientTypeRepository.java
@@ -0,0 +1,13 @@
+package com.art.repository;
+
+import com.art.model.ClientType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+@Repository
+public interface ClientTypeRepository extends JpaRepository<ClientType, Long> {
+}
