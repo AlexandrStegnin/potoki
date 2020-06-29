8d540c32f7a4e4534179cb9b32eec57dc15c8c0c
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-06-29 23:49:37 +0500

    Добавлены виды транзакций.

---
+++ src/main/java/com/art/model/supporting/enums/OperationType.java
@@ -0,0 +1,48 @@
+package com.art.model.supporting.enums;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+public enum OperationType {
+
+    UNDEFINED(0, "Не определено"),
+    DEBIT(1, "Дебет"),
+    CREDIT(2, "Кредит");
+
+    private final int id;
+
+    private final String title;
+
+    OperationType(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public static OperationType fromTitle(String title) {
+        for (OperationType value : values()) {
+            if (value.title.equalsIgnoreCase(title)) {
+                return value;
+            }
+        }
+        return UNDEFINED;
+    }
+
+    public static OperationType fromId(int id) {
+        for (OperationType value : values()) {
+            if (value.id == id) {
+                return value;
+            }
+        }
+        return UNDEFINED;
+    }
+
+}
