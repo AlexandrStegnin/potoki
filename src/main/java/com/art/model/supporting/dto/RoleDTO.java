2bb353a8a063ff40b76c0d111ce4d6180c81bb64
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-07-01 22:00:17 +0500

    Добавлен DTO роли пользователя.

---
+++ src/main/java/com/art/model/supporting/dto/RoleDTO.java
@@ -0,0 +1,16 @@
+package com.art.model.supporting.dto;
+
+import lombok.Data;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+@Data
+public class RoleDTO {
+
+    private Long id;
+
+    private String name;
+
+}
