d9768ebb4e041ca2a12d6f44abd62796a78b350a
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-07-01 22:01:24 +0500

    Добавлен DTO пользователя.

---
+++ src/main/java/com/art/model/supporting/dto/UserDTO.java
@@ -0,0 +1,37 @@
+package com.art.model.supporting.dto;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+import java.util.List;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UserDTO {
+
+    Long id;
+
+    String lastName;
+
+    String firstName;
+
+    String middleName;
+
+    String login;
+
+    String email;
+
+    List<RoleDTO> roles;
+
+    String kin;
+
+    Long partnerId;
+
+}
