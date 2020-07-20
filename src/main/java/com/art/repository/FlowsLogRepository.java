cfd72a7a2299e7e818375aca23efef07405e4155
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-07-20 23:00:20 +0500

    #DELETE. Удаляем неиспользуемые сущности/сервисы/интерфейсы

--- src/main/java/com/art/repository/FlowsLogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.art.repository;
-
-import com.art.model.supporting.FlowsLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FlowsLogRepository extends JpaRepository<FlowsLog, Long> {
-}
