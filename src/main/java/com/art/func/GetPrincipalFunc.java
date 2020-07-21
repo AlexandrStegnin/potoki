787b0d9765ce4e8f5d6a2445cd22ac7c573bcab7
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-07-21 16:53:42 +0500

    #DELETE. Рефакторинг. Удален сервис остановки приложения.

--- src/main/java/com/art/func/GetPrincipalFunc.java
+++ src/main/java/com/art/func/GetPrincipalFunc.java
@@ -2,21 +2,13 @@ package com.art.func;
 
 import com.art.model.AppRole;
 import com.art.model.SecurityUser;
-import com.art.model.supporting.UserFacilities;
-import com.art.service.FacilityService;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
-import java.math.BigInteger;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class GetPrincipalFunc {
-
-    @Resource(name = "facilityService")
-    private FacilityService facilityService;
 
     public String getLogin() {
         String userName;
@@ -28,25 +20,6 @@ public class GetPrincipalFunc {
             userName = principal.toString();
         }
         return userName;
-    }
-
-    public Long getPrincipalId() {
-        Long userId = Long.valueOf("0");
-        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-
-        if (principal instanceof SecurityUser) {
-            userId = ((SecurityUser) principal).getId();
-        }
-        return userId;
-    }
-
-    @Deprecated
-    public List<UserFacilities> getUserFacilities(Long userId) {
-        List<UserFacilities> userFacilities = new ArrayList<>(0);
-        UserFacilities defaultFacility = new UserFacilities(new BigInteger("0"), "Выберите объект");
-        userFacilities.add(defaultFacility);
-        userFacilities.addAll(facilityService.getInvestorsFacility(userId));
-        return userFacilities;
     }
 
     public boolean haveAdminRole() {
