1f1370823c2217ea98d983e0eae95a892f249db8
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-08-12 23:18:54 +0500

    Добавлен вид операции с деньгами инвесторов.

---
+++ src/main/java/com/art/model/supporting/enums/MoneyOperation.java
@@ -0,0 +1,50 @@
+package com.art.model.supporting.enums;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+public enum MoneyOperation {
+
+    UNDEFINED(0, "UNDEFINED"),
+    CREATE(1, "CREATE"),
+    EDIT(2, "EDIT"),
+    CLOSE(3, "CLOSE"),
+    DOUBLE(4, "DOUBLE");
+
+    private final int id;
+
+    private final String title;
+
+    MoneyOperation(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public static MoneyOperation fromId(int id) {
+        for (MoneyOperation operation : values()) {
+            if (operation.getId() == id) {
+                return operation;
+            }
+        }
+        return UNDEFINED;
+    }
+
+    public static MoneyOperation fromTitle(String title) {
+        for (MoneyOperation operation : values()) {
+            if (operation.getTitle().equalsIgnoreCase(title)) {
+                return operation;
+            }
+        }
+        return UNDEFINED;
+    }
+
+}
