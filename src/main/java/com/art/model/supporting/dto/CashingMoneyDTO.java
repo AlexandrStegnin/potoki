39b70593b6b71c2f7fae28a99a09ab279f0b7d99
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-08-16 12:33:45 +0500

    Добавлено DTO для вывода одиночной суммы

---
+++ src/main/java/com/art/model/supporting/dto/CashingMoneyDTO.java
@@ -0,0 +1,22 @@
+package com.art.model.supporting.dto;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * DTO для вывода одиночной суммы
+ *
+ * @author Alexandr Stegnin
+ */
+
+@Data
+public class CashingMoneyDTO {
+
+    private Long id;
+
+    private Date dateClosing;
+
+    private Date realDateGiven;
+
+}
