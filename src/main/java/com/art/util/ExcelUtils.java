e664e42cb920178886508d6cd6710db76bd1f76d
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-09-17 23:58:06 +0500

    Добавлен вспомогательный класс для загрузки excel файлов.

---
+++ src/main/java/com/art/util/ExcelUtils.java
@@ -0,0 +1,43 @@
+package com.art.util;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Вспомогательный класс для загрузки excel файлов
+ *
+ * @author Alexandr Stegnin
+ */
+
+public class ExcelUtils {
+
+    private ExcelUtils() {}
+
+    public static Workbook getWorkbook(InputStream inputStream, String excelPath) throws IOException {
+        Workbook workbook = null;
+        if (excelPath.endsWith("xlsx")) {
+            workbook = new XSSFWorkbook(inputStream);
+        } else if (excelPath.endsWith("xls")) {
+            workbook = new HSSFWorkbook(inputStream);
+        }
+        return workbook;
+    }
+
+    public static boolean isCorrect(Sheet sheet) {
+        int colCount = 0;
+        for (Row row : sheet) {
+            for (Cell cell : row) {
+                if (cell != null && cell.getCellTypeEnum() != CellType.BLANK) {
+                    colCount++;
+                }
+            }
+            break;
+        }
+        return colCount == 36;
+    }
+
+}
