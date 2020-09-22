bd75d054eed85b4bead3b34b567c0b5f1214fa6a
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-09-22 15:57:49 +0500

    Добавлены поля для DTO выплат (продажа)

--- src/main/java/com/art/model/supporting/dto/SalePaymentDTO.java
+++ src/main/java/com/art/model/supporting/dto/SalePaymentDTO.java
@@ -3,6 +3,7 @@ package com.art.model.supporting.dto;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -14,5 +15,13 @@ import java.util.List;
 public class SalePaymentDTO extends PaymentDTO {
 
     private List<Long> salePaymentsId;
+
+    private Date dateGiven;
+
+    private Long facilityId;
+
+    private Long underFacilityId;
+
+    private String shareType;
 
 }
