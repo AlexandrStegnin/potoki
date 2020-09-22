b6b377950462dadacf60d83d3e1e9927aca450d9
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-09-22 23:08:22 +0500

    Добавлена конфигурация для обновления маркетингового дерева по времени

--- src/main/java/com/art/service/ScheduledService.java
+++ src/main/java/com/art/service/ScheduledService.java
@@ -1,5 +1,6 @@
 package com.art.service;
 
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -7,7 +8,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class ScheduledService {
 
-    private MarketingTreeService marketingTreeService;
+    private final MarketingTreeService marketingTreeService;
 
     @Autowired
     public ScheduledService(final MarketingTreeService marketingTreeService) {
@@ -15,6 +16,9 @@ public class ScheduledService {
     }
 
     @Scheduled(cron = "0 0 1 * * ?")
+    @SchedulerLock(name = "ScheduledService_calculateMarketingTree",// уникальное имя задачи
+            lockAtLeastFor = "PT29M", // запускать не чаще, чем раз в 29 мин
+            lockAtMostFor = "PT29M") // если нода "умерла" и не отпустила локу, то держит её не более 29 мин
     public void calculateMarketingTree() {
         marketingTreeService.updateMarketingTreeFromApp();
     }
