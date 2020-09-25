d1b36e97d9600b18d06263481eae2f8f49c248b5
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-09-25 17:28:28 +0500

    Добавлен фильтр и спецификация для фильтров по пользователю

---
+++ src/main/java/com/art/model/supporting/filters/AppUserFilter.java
@@ -0,0 +1,18 @@
+package com.art.model.supporting.filters;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class AppUserFilter extends AbstractFilter {
+
+    private String login;
+
+    private boolean deactivated = false;
+
+}
---
+++ src/main/java/com/art/specifications/AppUserSpecification.java
@@ -0,0 +1,46 @@
+package com.art.specifications;
+
+import com.art.model.AppUser;
+import com.art.model.AppUser_;
+import com.art.model.UserProfile_;
+import com.art.model.supporting.filters.AppUserFilter;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.data.jpa.domain.Specifications.where;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+@Component
+public class AppUserSpecification extends BaseSpecification<AppUser, AppUserFilter> {
+
+    @Override
+    public Specification<AppUser> getFilter(AppUserFilter filter) {
+        return (root, query, cb) -> where(
+                loginEqual(filter.getLogin()))
+                .and(isDeactivated(filter.isDeactivated()))
+                .toPredicate(root, query, cb);
+    }
+
+    private static Specification<AppUser> loginEqual(String login) {
+        if (login == null || "Выберите инвестора".equalsIgnoreCase(login)) {
+            return null;
+        } else {
+            return ((root, criteriaQuery, criteriaBuilder) ->
+                    criteriaBuilder.equal(root.get(AppUser_.login), login)
+            );
+        }
+    }
+
+    private static Specification<AppUser> isDeactivated(boolean deactivated) {
+        if (!deactivated) {
+            return null;
+        }
+        return ((root, criteriaQuery, criteriaBuilder) ->
+                criteriaBuilder.equal(root.get(AppUser_.profile).get(UserProfile_.locked), true)
+        );
+    }
+
+}
