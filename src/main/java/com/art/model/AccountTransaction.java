ccf8397d3de20c73a10b8fe3f4c392d08de83d7a
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-10-08 17:27:47 +0500

    Добавлен признак блокировки суммы транзакции

--- src/main/java/com/art/model/AccountTransaction.java
+++ src/main/java/com/art/model/AccountTransaction.java
@@ -3,6 +3,7 @@ package com.art.model;
 import com.art.model.supporting.enums.CashType;
 import com.art.model.supporting.enums.OperationType;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
@@ -15,16 +16,9 @@ import java.util.Date;
 @Data
 @Entity
 @NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 @Table(name = "account_transaction")
-public class AccountTransaction {
-
-    @Id
-    @TableGenerator(name = "accountTransactionSeqStore", table = "SEQ_STORE",
-            pkColumnName = "SEQ_NAME", pkColumnValue = "ACCOUNT.TX.ID.PK",
-            valueColumnName = "SEQ_VALUE", allocationSize = 1)
-    @GeneratedValue(strategy = GenerationType.TABLE, generator = "accountTransactionSeqStore")
-    @Column(name = "id")
-    private Long id;
+public class AccountTransaction extends AbstractEntity {
 
     @Column(name = "tx_date")
     private Date txDate = new Date();
@@ -34,20 +28,31 @@ public class AccountTransaction {
     private OperationType operationType;
 
     @ManyToOne
-    @JoinColumn(name = "from_account_id")
-    private Account accountFrom;
+    @JoinColumn(name = "payer_account_id")
+    private Account payer;
 
     @OneToOne
     @JoinColumn(name = "owner_account_id")
     private Account owner;
 
+    @ManyToOne
+    @JoinColumn(name = "recipient_account_id")
+    private Account recipient;
+
     @OneToOne
     @JoinColumn(name = "sale_payment_id")
     private SalePayment salePayment;
 
+    @OneToOne
+    @JoinColumn(name = "money_id")
+    private Money money;
+
     @Enumerated(EnumType.ORDINAL)
     @Column(name = "cash_type_id")
     private CashType cashType;
+
+    @Column(name = "blocked")
+    private boolean blocked = false;
 
     public AccountTransaction(Account owner) {
         this.owner = owner;
