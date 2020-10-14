4b1287c2b4c34e08dd3bd6153e79bcefa6ddd114
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-10-14 14:40:31 +0500

    Добавлен контроллер для работы со свободными средствами клиентов.

---
+++ src/main/java/com/art/controllers/AccountTxSummaryController.java
@@ -0,0 +1,87 @@
+package com.art.controllers;
+
+import com.art.config.application.Location;
+import com.art.model.Facility;
+import com.art.model.UnderFacility;
+import com.art.model.supporting.dto.AccountTxDTO;
+import com.art.model.supporting.enums.ShareType;
+import com.art.model.supporting.filters.AccountTransactionFilter;
+import com.art.service.AccountTransactionService;
+import com.art.service.FacilityService;
+import com.art.service.UnderFacilityService;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.SortDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Контроллер для работы со свободными средствами клиентов
+ *
+ * @author Alexandr Stegnin
+ */
+
+@Controller
+public class AccountTxSummaryController {
+
+    private final AccountTransactionService accountTransactionService;
+
+    private final AccountTransactionFilter filter = new AccountTransactionFilter();
+
+    private final FacilityService facilityService;
+
+    private final UnderFacilityService underFacilityService;
+
+    public AccountTxSummaryController(AccountTransactionService accountTransactionService, FacilityService facilityService,
+                                      UnderFacilityService underFacilityService) {
+        this.accountTransactionService = accountTransactionService;
+        this.facilityService = facilityService;
+        this.underFacilityService = underFacilityService;
+    }
+
+    @GetMapping(path = Location.TRANSACTIONS_SUMMARY)
+    public ModelAndView accountsPage(@PageableDefault(size = 100) @SortDefault Pageable pageable) {
+        return prepareModel(filter);
+    }
+
+    @PostMapping(path = Location.TRANSACTIONS_SUMMARY)
+    public ModelAndView accountsPageFiltered(@ModelAttribute("filter") AccountTransactionFilter filter) {
+        return prepareModel(filter);
+    }
+
+    private ModelAndView prepareModel(AccountTransactionFilter filter) {
+        ModelAndView model = new ModelAndView("free-cash");
+        Pageable pageable = new PageRequest(filter.getPageNumber(), filter.getPageSize());
+        model.addObject("page", accountTransactionService.getSummary(filter, pageable));
+        model.addObject("filter", filter);
+        model.addObject("accountTxDTO", new AccountTxDTO());
+        return model;
+    }
+
+    @ModelAttribute("owners")
+    public List<String> initOwners() {
+        return accountTransactionService.initOwners();
+    }
+
+    @ModelAttribute("facilities")
+    public List<Facility> initializeFacilities() {
+        return facilityService.initializeFacilities();
+    }
+
+    @ModelAttribute("underFacilities")
+    public List<UnderFacility> initializeUnderFacilities() {
+        return underFacilityService.initializeUnderFacilities();
+    }
+
+    @ModelAttribute("shareTypes")
+    public List<ShareType> initializeShareTypes() {
+        return Arrays.asList(ShareType.values());
+    }
+}
