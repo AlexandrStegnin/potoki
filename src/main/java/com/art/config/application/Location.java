7e4281ec24591f64498cc9bba1e87490c492520a
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-10-16 19:33:02 +0500

    Добавлен путь для доступа к странице маркетингового дерева

--- src/main/java/com/art/config/application/Location.java
+++ src/main/java/com/art/config/application/Location.java
@@ -244,6 +244,9 @@ public class Location {
 
     public static final String TRANSACTIONS_REINVEST = ACC_TRANSACTIONS + "/reinvest";
 
+    //МАРКЕТИНГОВОЕ ДЕРЕВО
+    public static final String MARKETING_TREE = "/marketing-tree";
+
     public static final String[] ADMIN_URLS = {
             ADMIN,
             CATALOGUE,
