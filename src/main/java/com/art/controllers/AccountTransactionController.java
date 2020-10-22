8c8dc17d9d658e7ba1a77bc04863189af1dfae3c
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-10-22 17:16:43 +0500

    Добавлены атрибуты для заполнения выпадающих списков

--- src/main/java/com/art/controllers/AccountTransactionController.java
+++ src/main/java/com/art/controllers/AccountTransactionController.java
@@ -3,12 +3,17 @@ package com.art.controllers;
 import com.art.config.application.Location;
 import com.art.model.Account;
 import com.art.model.AccountTransaction;
+import com.art.model.Facility;
+import com.art.model.UnderFacility;
 import com.art.model.supporting.ApiResponse;
 import com.art.model.supporting.dto.AccountTxDTO;
 import com.art.model.supporting.dto.BalanceDTO;
 import com.art.model.supporting.enums.CashType;
+import com.art.model.supporting.enums.ShareType;
 import com.art.model.supporting.filters.AccTxFilter;
 import com.art.service.AccountTransactionService;
+import com.art.service.FacilityService;
+import com.art.service.UnderFacilityService;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -21,6 +26,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -35,8 +41,15 @@ public class AccountTransactionController {
 
     private final AccTxFilter filter = new AccTxFilter();
 
-    public AccountTransactionController(AccountTransactionService transactionService) {
+    private final FacilityService facilityService;
+
+    private final UnderFacilityService underFacilityService;
+
+    public AccountTransactionController(AccountTransactionService transactionService,
+                                        FacilityService facilityService, UnderFacilityService underFacilityService) {
         this.transactionService = transactionService;
+        this.facilityService = facilityService;
+        this.underFacilityService = underFacilityService;
     }
 
     /**
@@ -60,18 +73,6 @@ public class AccountTransactionController {
     public ModelAndView accountTransactionsFiltered(@ModelAttribute(value = "filter") AccTxFilter filter) {
         return prepareModel(filter);
     }
-
-    /**
-     * Получить отфильтрованную страницу для отображения списка транзакций по счетам клиентов
-     *
-     * @param dto для фильтрации результата
-     * @return страница
-     */
-//    @ResponseBody
-//    @PostMapping(path = Location.ACC_TRANSACTIONS_POPUP)
-//    public List<AccountTransactionDTO> accountTransactionsPopup(@RequestBody AccountSummaryDTO dto) {
-//        return transactionService.fetch(dto);
-//    }
 
     /**
      * Удалить суммы транзакций
@@ -130,6 +131,21 @@ public class AccountTransactionController {
     @ModelAttribute("parentPayers")
     public List<Account> initParentPayers() {
         return transactionService.initParentPayers();
+    }
+
+    @ModelAttribute("facilities")
+    public List<Facility> initFacilities() {
+        return facilityService.initializeFacilities();
+    }
+
+    @ModelAttribute("underFacilities")
+    public List<UnderFacility> initUnderFacilities() {
+        return underFacilityService.initializeUnderFacilities();
+    }
+
+    @ModelAttribute("shareTypes")
+    public List<ShareType> initShareTypes() {
+        return Arrays.asList(ShareType.values());
     }
 
     @InitBinder
