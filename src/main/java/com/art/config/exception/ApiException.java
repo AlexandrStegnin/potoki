e2b341060ec550a262fe5db5554b3c0278267f91
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2021-04-16 11:04:33 +0500

    Доработано собственное исключение

--- src/main/java/com/art/config/exception/ApiException.java
+++ src/main/java/com/art/config/exception/ApiException.java
@@ -1,6 +1,7 @@
 package com.art.config.exception;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 /**
  * @author Alexandr Stegnin
@@ -11,8 +12,11 @@ public class ApiException extends RuntimeException {
 
     private final String message;
 
-    public ApiException(String message) {
+    private final HttpStatus status;
+
+    public ApiException(String message, HttpStatus status) {
         super(message);
+        this.status = status;
         this.message = message;
     }
 
