d44b70f06dc5205194bdf7a5ed42d63aa2b43fd9
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2021-04-21 17:13:35 +0500

    Добавлено перечисление для хранения названий страниц приложения.

---
+++ src/main/java/com/art/model/supporting/enums/AppPage.java
@@ -0,0 +1,40 @@
+package com.art.model.supporting.enums;
+
+import lombok.Getter;
+
+import java.util.Objects;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+@Getter
+public enum AppPage {
+
+    MONEY(1, "Деньги инвесторов", "money");
+
+    private final int id;
+
+    private final String title;
+
+    private final String page;
+
+    AppPage(int id, String title, String page) {
+        this.id = id;
+        this.title = title;
+        this.page = page;
+    }
+
+    public static AppPage fromId(Integer id) {
+        if (Objects.isNull(id)) {
+            return null;
+        }
+        for (AppPage page : values()) {
+            if (page.getId() == id) {
+                return page;
+            }
+        }
+        return null;
+    }
+
+}
