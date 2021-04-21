c03b733d95468a5bb5b86e3c50c6bc9487516911
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2021-04-21 17:07:22 +0500

    Добавлен класс для хранения фильтров, которые последний раз использовались на странице.

---
+++ src/main/java/com/art/model/supporting/AppFilter.java
@@ -0,0 +1,37 @@
+package com.art.model.supporting;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Класс для хранения в базе фильтров,
+ * которые в последний раз использовались
+ *
+ * @author Alexandr Stegnin
+ */
+
+@Data
+@Entity
+@Table(name = "app_filter")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class AppFilter {
+
+    @Id
+    Long id;
+
+    @Column(name = "user_id")
+    Long userId;
+
+    @Column(name = "page_name")
+    String pageName;
+
+    @Column(name = "text")
+    String text;
+
+}
