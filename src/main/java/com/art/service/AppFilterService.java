fc0cedae9c881943ff074d170ba84337e0f18336
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2021-04-28 14:33:28 +0500

    Добавлен метод получения фильтра из базы данных.

--- src/main/java/com/art/service/AppFilterService.java
+++ src/main/java/com/art/service/AppFilterService.java
@@ -1,20 +1,31 @@
 package com.art.service;
 
+import com.art.config.SecurityUtils;
 import com.art.model.supporting.AppFilter;
+import com.art.model.supporting.enums.AppPage;
+import com.art.model.supporting.filters.AbstractFilter;
 import com.art.repository.AppFilterRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 /**
  * @author Alexandr Stegnin
  */
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class AppFilterService {
+
+    ObjectMapper objectMapper;
 
     AppFilterRepository appFilterRepository;
 
@@ -24,5 +35,18 @@ public class AppFilterService {
 
     public void save(AppFilter appFilter) {
         appFilterRepository.save(appFilter);
+    }
+
+    public AbstractFilter getFilter(AbstractFilter oldFilter, Class<? extends AbstractFilter> clazz, AppPage page) {
+        Long curUserId = SecurityUtils.getUserId();
+        AppFilter appFilter = findFilter(curUserId, page.getId());
+        if (Objects.nonNull(appFilter)) {
+            try {
+                return objectMapper.readValue(appFilter.getText(), clazz);
+            } catch (JsonProcessingException e) {
+                log.trace("Не удалось получить фильтр: " + e.getMessage());
+            }
+        }
+        return oldFilter;
     }
 }
