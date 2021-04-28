631cf4ddf3c65bb8d7b840211ab40717c21a730d
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2021-04-28 14:01:16 +0500

    Добавлены конструкторы

--- src/main/java/com/art/model/supporting/filters/CashFilter.java
+++ src/main/java/com/art/model/supporting/filters/CashFilter.java
@@ -1,14 +1,18 @@
 package com.art.model.supporting.filters;
 
+import com.art.model.AppUser;
 import com.art.model.Facility;
 import com.art.model.UnderFacility;
-import com.art.model.AppUser;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class CashFilter extends AbstractFilter {
     private AppUser investor;
