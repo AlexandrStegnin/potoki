5095b06ca70eaedc48f7bfb73d967588352c4144
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2021-04-30 14:12:42 +0500

    Добавлена страница для работы с деталями новых денег. Добавлены конструкторы.

--- src/main/java/com/art/model/supporting/dto/NewCashDetailDTO.java
+++ src/main/java/com/art/model/supporting/dto/NewCashDetailDTO.java
@@ -1,16 +1,24 @@
 package com.art.model.supporting.dto;
 
+import com.art.model.NewCashDetail;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author Alexandr Stegnin
  */
 
 @Data
+@NoArgsConstructor
 public class NewCashDetailDTO {
 
     private Long id;
 
     private String name;
+
+    public NewCashDetailDTO(NewCashDetail entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+    }
 
 }
